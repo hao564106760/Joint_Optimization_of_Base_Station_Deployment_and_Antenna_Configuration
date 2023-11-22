6c723d45dedfd736eabd96fccbedf0c97357a2f3
hao564106760 <564106760@qq.com>
2023-11-22 21:53:56 +0800

    Add files via upload

---
+++ envs/pycomm/protos/wolong/config/v1/config_pb2.py
@@ -0,0 +1,16 @@
+
+'Generated protocol buffer code.'
+from google.protobuf.internal import builder as _builder
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import symbol_database as _symbol_database
+_sym_db = _symbol_database.Default()
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1dwolong/config/v1/config.proto\x12\x10wolong.config.v1"$\n\tMongoPath\x12\n\n\x02db\x18\x01 \x01(\t\x12\x0b\n\x03col\x18\x02 \x01(\t"\x1b\n\x0cOutputTarget\x12\x0b\n\x03sql\x18\x01 \x01(\tb\x06proto3')
+_builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'wolong.config.v1.config_pb2', globals())
+if (_descriptor._USE_C_DESCRIPTORS == False):
+    DESCRIPTOR._options = None
+    _MONGOPATH._serialized_start = 51
+    _MONGOPATH._serialized_end = 87
+    _OUTPUTTARGET._serialized_start = 89
+    _OUTPUTTARGET._serialized_end = 116
